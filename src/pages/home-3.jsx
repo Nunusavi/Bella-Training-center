66a777193daef9f338543a8a0cffbc62f5e107e3
nunusavi <nathanmesfin919@gmail.com>
2025-03-25 12:29:11 +0300

    minor changes on wrapper

--- src/pages/home-3.jsx
+++ src/pages/home-3.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import SEO from '../common/seo';
-import HomeThree from '../components/homes/home-3';
-import WrapperThree from '../layout/wrapper-3';
+import React from "react";
+import SEO from "../common/seo";
+import HomeThree from "../components/homes/home-3";
+import WrapperThree from "../layout/wrapper";
 
 const index = () => {
-    return (
-        <WrapperThree>
-            <SEO pageTitle={"Home 3"} />
-            <HomeThree />
-        </WrapperThree>
-    );
+  return (
+    <WrapperThree>
+      <SEO pageTitle={"Home 3"} />
+      <HomeThree />
+    </WrapperThree>
+  );
 };
 
 export default index;
